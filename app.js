68b23433506e870d8853a6fee0d54b50d014c197
Won Wook SONG <wsong0512@gmail.com>
2015-07-30 22:10:35 +0900

    The Posts Page

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ function($stateProvider, $urlRouterProvider) {
 		url: '/home',
 		templateUrl: '/home.html',
 		controller: 'MainCtrl'
+	})
+	.state('posts', {
+		url: '/posts/{id}',
+		templateUrl: '/posts.html',
+		controller: 'PostsCtrl'
 	});
 
 	$urlRouterProvider.otherwise('home');
@@ -31,12 +36,38 @@ function($scope, posts){
 
 	$scope.addPost = function() {
 		if(!$scope.title || $scope.title === '') { return; }
-		$scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});
+		$scope.posts.push({
+			title: $scope.title,
+			link: $scope.link,
+			upvotes: 0,
+			comments: [
+				{author: 'Joe', body: 'Cool post!', upvotes: 0},
+				{author: 'Bob', body: 'Great idea!', upvotes: 0}
+			]
+		});
 		$scope.title = '';
 		$scope.link = '';
 	};
 
 	$scope.incrementUpvotes = function(post) {
 		post.upvotes += 1;
+	};
+}]);
+
+app.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts){
+	$scope.post = posts.posts[$stateParams.id];
+
+	$scope.addComment = function() {
+		if($scope.body === '') { return; }
+		$scope.post.comments.push({
+			body: $scope.body,
+			author: 'user',
+			upvotes: 0
+		});
+		$scope.body = '';
 	};
 }]);
