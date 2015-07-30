c2aee6960a2928b3cb5278883f2770084f1d9134
Won Wook SONG <wsong0512@gmail.com>
2015-07-30 17:02:24 +0900

    Angular Services

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 var app = angular.module('flapperNews', []);
 
+app.factory('posts', [function() {
+	var o = {
+		posts: []
+	};
+	return o;
+}]);
+
 app.controller('MainCtrl', [
 '$scope',
-function($scope){
+'posts',
+function($scope, posts){
 	$scope.test = 'Hello world!';
 
-	$scope.posts = [
-		{title: 'post 1', upvotes: 5},
-		{title: 'post 2', upvotes: 2},
-		{title: 'post 3', upvotes: 16},
-		{title: 'post 4', upvotes: 9},
-		{title: 'post 5', upvotes: 4}
-	];
+	$scope.posts = posts.posts;
 
 	$scope.addPost = function() {
 		if(!$scope.title || $scope.title === '') { return; }
