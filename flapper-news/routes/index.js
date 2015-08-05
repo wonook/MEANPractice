20834198f878d29fb93cd8ac775382f87335b6cc
Won Wook SONG <wsong0512@gmail.com>
2015-08-05 20:41:48 +0900

    Opening REST Routes - get/post methods for posts

--- flapper-news/routes/index.js
+++ flapper-news/routes/index.js
@@ -6,4 +6,42 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+var mongoose = require('mongoose');
+var Post = mongoose.model('Post');
+var Comment = mongoose.model('Comment');
+
+router.get('/posts', function(req, res, next) {
+	Post.find(function(err, posts) {
+		if(err) { return nect(err); }
+
+		res.json(posts);
+	});
+});
+
+router.post('/posts', function(req, res, next) {
+	var post = new Post(req.body);
+
+	post.save(function(err, post) {
+		if(err){ return next(err); }
+
+		res.json(post);
+	});
+});
+
+router.param('post', function(req, res, next, id) {
+	var query = Post.findById(id);
+
+	query.exec(function(err, post) {
+		if (err) { return next(err); }
+		if (!post) { return next(new Error('can\'t find post')); }
+
+		req.post = post;
+		return next;
+	});
+});
+
+router.get('/posts/:post', function(req, res) {
+	res.json(req.post);
+});
+
 module.exports = router;
